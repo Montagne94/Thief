3ff2d913ca483f15051ab52b615e55d9249157d3
Montagne94 <77510722+Montagne94@users.noreply.github.com>
2021-01-18 21:11:15 +0300

    Thief

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.right * _speed * Time.deltaTime);
+            _spriteRenderer.flipX = false;
+            _animator.Play("run");
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate((transform.right * _speed) * -1 * Time.deltaTime);
+            _spriteRenderer.flipX = true;
+            _animator.Play("run");
+        }
+        else
+        {
+            _animator.Play("idle");
+        }
+    }
+}
---
+++ Scripts/Signaling.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Signaling : MonoBehaviour
+{
+    private float _volumeChangeStep;
+    private AudioSource _signaling;
+
+    private void Start()
+    {
+        _signaling = GetComponent<AudioSource>();
+        _volumeChangeStep = .01f;
+        _signaling.volume = 0;
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            StopAllCoroutines();
+            StartCoroutine(AddVolume());
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            StopAllCoroutines();
+            StartCoroutine(RemoveVolume());
+        }
+    }
+
+    private IEnumerator AddVolume()
+    {
+        _signaling.Play();
+        while(_signaling.volume < 1)
+        {
+        _signaling.volume += _volumeChangeStep;
+        yield return null;
+        }
+    }
+
+    private IEnumerator RemoveVolume()
+    {
+        while (_signaling.volume > 0)
+        {
+            _signaling.volume -= _volumeChangeStep;
+            yield return null;
+        }
+        _signaling.Stop();
+    }
+}
